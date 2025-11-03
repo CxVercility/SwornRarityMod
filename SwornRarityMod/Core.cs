52f506781b4f8d74a26fe8c79a8426ffc69202d9
CxVercility <vercility@gmail.com>
2025-11-04 00:26:40 +0100

    Add project files.

---
+++ SwornRarityMod/Core.cs
@@ -0,0 +1,33 @@
+﻿using Il2Cpp;
+using MelonLoader;
+using HarmonyLib;
+
+[assembly: MelonInfo(typeof(SwornRarityMod.Core), "SwornRarityMod", "1.0.0", "Vercility", null)]
+[assembly: MelonGame("Windwalk Games", "SWORN")]
+
+namespace SwornRarityMod
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
+[HarmonyPatch(typeof(BlessingGenerator), "GenerateBlessings")]
+class Patch
+{
+    private static Boolean hasRun = false;
+    static void Prefix(BlessingGenerator __instance)
+    {
+        if (!hasRun)
+        {
+            __instance.legendaryBlessingBaseChance = 0.05f;
+            __instance.epicBlessingBaseChance = 0.08f;
+            __instance.rareBlessingBaseChance = 0.12f;
+            __instance.uncommonBlessingBaseChance = 0.25f;
+        }
+        
+    }
+}
